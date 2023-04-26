5dea2e88b65a5a7973ad9f8d962e27036db08b9e
dalealexandersingh <dale.alexander.singh@gmail.com>
2023-04-26 08:40:02 +0200

    initial changes

---
+++ toast.js
@@ -0,0 +1,50 @@
+﻿
+
+function ShowToastInfo(message){
+	AddToast(message, "toast-info");
+}
+
+function ShowToastError(message){
+	AddToast(message, "toast-error");
+}
+
+function ShowToastWarn(message){
+	AddToast(message, "toast-warn");
+}
+
+function AddToast(message, className){
+	
+	if (!window.ToastMessages) {
+		window.ToastMessages = [];
+	}
+	
+	window.ToastMessages.push({message: message, className: className})
+	
+	if (!window.ToastTimer) {
+		var toast = window.ToastMessages.pop();
+		ShowToast(toast.message, toast.className);
+		window.ToastTimer = setInterval(function() {
+			if (window.ToastMessages.length > 0) {
+				var toast = window.ToastMessages.pop();
+				ShowToast(toast.message, toast.className);
+			}
+			if (window.ToastMessages.length == 0) {
+				clearInterval(window.ToastTimer);
+				window.ToastTimer = null;
+			}
+		}, 2000);
+	}
+
+}
+
+function ShowToast(message, className){
+
+	if (window.Toast) {
+		document.body.removeChild(window.Toast);
+	}
+
+	window.Toast = document.createElement('div');
+	window.Toast.innerHTML = message;
+	window.Toast.className = "toast " + className;
+	document.body.appendChild(window.Toast);
+}
